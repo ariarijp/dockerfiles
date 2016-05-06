2ee38d13431d9d5cc8273cfb8b4a114447ca374a
ariarijp <takuya.arita@gmail.com>
2016-05-06 21:41:11 +0900

    Update botkit

--- botkit/bot.js
+++ botkit/bot.js
@@ -1,23 +1,42 @@
-var Botkit = require('botkit');
+var Botkit = require('botkit')
 
-var token = process.env.SLACK_API_TOKEN;
+var token = process.env.SLACK_API_TOKEN
 if (!token) {
-    throw 'SLACK_API_TOKEN is required.';
+    throw 'SLACK_API_TOKEN is required.'
 }
 
-var debug = false;
+var debug = false
 if (process.env.DEBUG) {
-    debug = true;
+    debug = true
 }
 
 var controller = Botkit.slackbot({
     debug: debug
-});
+})
 
 controller.spawn({
-    token: token,
+    token: token
 }).startRTM()
 
+controller.setupWebserver(process.env.PORT, function(err, webserver) {
+    if (err) {
+        console.error(err)
+        process.exit(1)
+    }
+
+    controller.createWebhookEndpoints(webserver)
+})
+
 controller.hears('hello', ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
-    bot.reply(message, 'hello');
-});
+    bot.reply(message, 'hello')
+})
+
+controller.on('slash_command', function (bot, message) {
+  switch (message.command) {
+    case '/hello':
+      bot.replyPrivate(message, 'hello')
+      break
+    default:
+      break;
+  }
+})
