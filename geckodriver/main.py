293c5da38f0c2d420f1ecd42ff283e014b1c92c1
ariarijp <takuya.arita@gmail.com>
2017-04-07 00:01:34 +0900

    Add geckodriver

---
+++ geckodriver/main.py
@@ -0,0 +1,11 @@
+import time
+from selenium import webdriver
+
+driver = webdriver.Firefox()
+driver.get('http://www.google.com/xhtml')
+time.sleep(5)
+search_box = driver.find_element_by_name('q')
+search_box.send_keys('GeckoDriver')
+search_box.submit()
+time.sleep(5)
+driver.quit()
